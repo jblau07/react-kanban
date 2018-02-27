47e2c01945a5f91399aed8aada532f3a3500406d
jblau07 <jlau7248@gmail.com>
2018-02-26 16:58:52 -1000

    refactored code

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import CardListItem from './CardListItem';
 
-const CardList = ({ cards }) => {
+const CardList = ({ cards, status }) => {
   return (
     <div>
-        {cards.map((card) => {
+        {cards.filter(card => {
+          return card.status.toLowerCase() === status
+        })
+        .map((card) => {
           return (
             <CardListItem
             key={card.id}
-            title={card.title}
-            priority={card.priority}
-            status={card.status}
-            created_by={card.created_by}
-            assigned_to={card.assigned_to}
+           {...card}
           />)
         }
         )
--- src/components/CardListItem.js
+++ src/components/CardListItem.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
 export const CardListItem = (props) => {
+  const {title, priority, status, created_by, assigned_to} = props
   return (
     <div className="card">
-      <div className="attribute">Title:{props.title}</div>
-      <div className="attribute">Priority:{props.priority}</div>
+      <div className="attribute">Title:{title}</div>
+      <div className="attribute">Priority:{priority}</div>
       {/* <div className="attribute">Status:{props.status}</div> */}
-      <div className="attribute">Created By:{props.created_by}</div>
-      <div className="attribute">Assigned To:{props.assigned_to}</div>
+      <div className="attribute">Created By:{created_by}</div>
+      <div className="attribute">Assigned To:{assigned_to}</div>
     </div>
   );
 }
