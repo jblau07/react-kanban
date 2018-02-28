e22f0f6f299340b41f8b2ec98b9b4cd7ca0eba87
jblau07 <jlau7248@gmail.com>
2018-02-28 12:17:50 -1000

    modified to include edit and delete buttons

---
+++ src/containers/CardListItem.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { setEditId, deleteCard } from '../actions';
+
+class CardListItem extends Component {
+  constructor(props) {
+    super (props);
+    this.handleOnEdit = this.handleOnEdit.bind(this);
+    this.handleOnDelete = this.handleOnDelete.bind(this);
+
+  }
+  
+  handleOnEdit() {
+    this.props.setEditId(this.props.id)
+  }
+
+  handleOnDelete() {
+    this.props.deleteCard(this.props.id)
+  }
+  
+  render() {
+    const { title, priority, created_by, assigned_to } = this.props
+      return (
+        <div className="card">
+          <div className="attribute">Title:{title}</div>
+          <div className="attribute">Priority:{priority}</div>
+          <div className="attribute">Created By:{created_by}</div>
+          <div className="attribute">Assigned To:{assigned_to}</div>
+          <button onClick={this.handleOnEdit}>Edit</button> 
+          <button onClick={this.handleOnDelete}>Delete</button> 
+
+        </div>
+      )
+    }
+  }
+
+const mapStateToProps = state => {
+  return {
+    cards: state.cards.cards,
+    editId: state.cards.editId
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    setEditId: function (id) {
+      dispatch(setEditId(id))
+    },
+    deleteCard: function (id) {
+      dispatch(deleteCard(id))
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CardListItem);
+
+
