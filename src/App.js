e058c8ecf2930318bdb786a316901323a6b1fbd8
jblau07 <jlau7248@gmail.com>
2018-03-28 14:48:57 -1000

    added classnames and edited some text

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 // import './style.css';
 import { connect } from 'react-redux';
 import './index.css';
-import { loadCards } from '../src/actions/index';
+import { loadCards } from '../src/actions';
 import CardList from './components/CardList';
 import NewCard from './containers/NewCard';
 import UpdateCard from './containers/UpdateCard';
@@ -20,26 +20,26 @@ class App extends Component {
     return (
       <div className="App">
       <header className="App-title">
-      <h1>Kanban  </h1>
+      <h1>React-Kanban  </h1>
         <NewCard/>
         <UpdateCard 
        />
       </header>
         <div className="Main">
           <div className="QueueColumn">
-          <h1>Queue</h1>
+          <h1 className="queue-header">Queue</h1>
           <CardList
             cards={this.props.cards} status='queue'
           />
           </div>
           <div className="In-ProgressColumn">
-          <h1>In-Progress</h1>
+          <h1 className="in-progress-header">In-Progress</h1>
           <CardList
             cards={this.props.cards} status="in-progress"
           />
           </div>
           <div className="DoneColumn">
-          <h1>Done</h1>
+          <h1 className="done-header">Done</h1>
           <CardList
             cards={this.props.cards} status="done"
           />
